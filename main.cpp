d80c1f354dec2e4459a1b2681543640192abe5ba
kenils <shahkenil20@gmail.com>
2021-04-02 11:16:31 +0530

    computer graphics project

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: abc
+ *
+ * Created on April 13, 2019, 1:07 PM
+ */
+
+#include <cstdlib>
+#include<stdio.h>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello World");
+    return 0;
+}
+
